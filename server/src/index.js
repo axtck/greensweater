3dae577eb816a4ca75a5e98d9d6bdcc175f18711
axtck <alexandertack@hotmail.be>
2021-08-06 23:20:55 +0200

    Update mysql package and fix db connection

--- server/src/index.js
+++ server/src/index.js
@@ -1,14 +1,27 @@
-const express = require('express');
-const morgan = require('morgan');
-const helmet = require('helmet');
-const cors = require('cors');
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const cors = require("cors");
+const mysql = require("mysql2");
 
 require('dotenv').config();
 
-//const middlewares = require('./middlewares');
 const api = require('./api');
 
 const app = express();
+
+const db = mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
+});
+
+db.connect((err) => {
+    if (err) return console.log(err);
+    console.log("Connected");
+});
 
 app.use(morgan("dev"));
 app.use(helmet());
@@ -22,4 +35,5 @@ app.get("/", (req, res) => res.json({
 app.use("/api/v1", api);
 
 const port = process.env.PORT || 5000;
+
 app.listen(port, () => console.log(`Listening (${port})`));

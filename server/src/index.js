42842fb74e08015aa44ff09ebd359d3f6c33798f
axtck <alexandertack@hotmail.be>
2021-08-26 15:40:28 +0200

    Update db name in connection string

--- server/src/index.js
+++ server/src/index.js
@@ -14,14 +14,15 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json()); // parse requests of content-type - application/json
 
-// get env vars
+// connection string vars
+const mdbName = "greensweaterdb";
 const mdbHost = process.env.MONGO_HOST;
 const mdbPort = process.env.MONGO_PORT;
 const mdbPassword = process.env.MONGO_PASSWORD;
 const mdbUser = process.env.MONGO_USER;
 
 // setup connection string
-const mongoUrl = `mongodb://${mdbUser}:${mdbPassword}@${mdbHost}:${mdbPort}/?authSource=admin`;
+const mongoUrl = `mongodb://${mdbUser}:${mdbPassword}@${mdbHost}:${mdbPort}/${mdbName}?authSource=admin`;
 
 // connect using mongoose 
 db.mongoose
